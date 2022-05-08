61c84802fc94d7fd249ebc2abffcff418b7ec09f
XUXU <corpochta@gmail.com>
2022-05-08 18:29:07 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/ru/job4j/condition/TgrArea.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class TgrArea {
+    @SuppressWarnings("checkstyle:WhitespaceAround")
+    public static double area(double a, double b, double c) {
+        double rsl = Math.sqrt(3 * (3 - a) * (3 - b) * (3 - c));
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        double rsl = TgrArea.area(2, 2, 2);
+        System.out.println("area (2, 2, 2) = " + rsl);
+    }
+}
+
