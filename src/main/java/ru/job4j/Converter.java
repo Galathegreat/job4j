2e78dbf42e4a73d8bb742de120b83002380de81c
XUXU <corpochta@gmail.com>
2022-05-10 16:31:29 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/ru/job4j/Converter.java
+++ src/main/java/ru/job4j/Converter.java
@@ -2,18 +2,19 @@ package ru.job4j;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-        float rsl = value / 70;
-        return rsl;
+        float result = value / 70;
+        return result;
     }
 
     public static float rubleToDollar(float value) {
-        float rsl = value/60;
-        return rsl;
+        float result = value / 60;
+        return result;
     }
 
     public static void main(String[] args) {
+
         float euro = Converter.rubleToEuro(140);
-        float dollar = Converter.rubleToDollar (140);
+        float dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("140 rubles are " + dollar + " dollar.");
 
@@ -24,4 +25,5 @@ public class Converter {
             System.out.println("140 rubles are 2. Test result : " + passed);
         }
     }
+
 
