5be481df2d397a78f78616dd5ca630918e0a61bc
XUXU <corpochta@gmail.com>
2022-05-15 16:46:20 +0300

    Merge remote-tracking branch 'origin/master'

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -6,13 +6,13 @@ import org.junit.Test;
 public class X2Test {
 
     @Test
-    public void whenA10B0C0X2Then40 () {
+    public void whenA10B0C0X2Then40() {
     int a = 1;
     int b = 1;
     int c = 1;
     int x = 0;
     int expected = 1;
-    int rsl = X2.calc(a,b,c,x);
+    int rsl = X2.calc(a, b, c, x);
     Assert.assertEquals(expected, rsl);
     }
 }
