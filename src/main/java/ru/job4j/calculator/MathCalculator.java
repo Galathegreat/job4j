951b8f92c7a9b18c764c630e31ff5f0d2e94a8a8
XUXU <corpochta@gmail.com>
2022-05-16 16:45:58 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -3,11 +3,20 @@ package ru.job4j.calculator;
 import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
-    public static double sumAndMultiply(double first, double second) {
-        return sum(first, second);
+    public static double subAndDiv(double first, double second) {
+        return sub(first, second)
+                + div(first, second);
     }
 
-    public static void main(String[] args) {
-        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+    public static double sumAllMethods(double first, double second) {
+        return multiply(first, second)
+                + sum(first, second)
+                + sub(first, second)
+                + div(first, second);
     }
-}
+
+        public static void main(String[] args) {
+            System.out.println("Результат расчета равен: " + sumAllMethods(10, 20));
+            System.out.println("Результат расчета равен: " + subAndDiv(10, 20));
+        }
+    }
