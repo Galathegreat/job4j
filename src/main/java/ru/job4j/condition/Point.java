eb0b1a34812fc17996bc81fc8e5037dc7e608995
XUXU <corpochta@gmail.com>
2022-05-17 11:04:01 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -6,7 +6,7 @@ public class Point {
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
+        double result = Point.distance(0, 1, 2, 1);
+        System.out.println("result (0, 1) to (2, 1) " + result);
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,12 +8,12 @@ import static org.junit.Assert.*;
 public class PointTest {
 
     @Test
-    public void when00to20then2() {
+    public void when01to21then2() {
         double expected = 2;
         int x1 = 0;
-        int y1 = 0;
+        int y1 = 1;
         int x2 = 2;
-        int y2 = 0;
+        int y2 = 1;
         double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
     }
