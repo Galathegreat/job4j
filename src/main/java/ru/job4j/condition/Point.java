eaf3c7166e2448c5f48f5fc71e5e1d2b5ea63119
XUXU <corpochta@gmail.com>
2022-05-17 11:07:13 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -6,7 +6,7 @@ public class Point {
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 1, 2, 1);
-        System.out.println("result (0, 1) to (2, 1) " + result);
+        double result = Point.distance(1, 3, 3, 1);
+        System.out.println("result 1, 3) to (3, 1) " + result);
     }
 }
