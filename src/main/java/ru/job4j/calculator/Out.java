2e6b6be1799ad34aa36e961e6f4a31fcea696747
XUXU <corpochta@gmail.com>
2022-05-17 11:55:36 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/ru/job4j/calculator/Out.java
+++ src/main/java/ru/job4j/calculator/Out.java
@@ -4,9 +4,9 @@ public class Out {
     public static void main(String[] args) {
        String idea = "I like Java";
        System.out.println(idea);
+       idea = idea + " " + "But I am a newbie";
        int year = 2022;
-       String separate = " ";
-       String id = idea + " " + "But I am a newbie" + " " + year;
-        System.out.println(id);
+       idea += year;
+        System.out.println(idea);
     }
 }
