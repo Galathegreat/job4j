a0f3dffb645aa2141a7ac76291ecd6deff7394e5
XUXU <corpochta@gmail.com>
2022-05-17 16:51:18 +0300

    Merge remote-tracking branch 'origin/master'

--- src/test/java/ru/job4j/condition/DummyBotTest.java
+++ src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -20,7 +20,7 @@ public class DummyBotTest {
     }
 
     @Test
-public void whenUnknownBot () {
+public void whenUnknownBot() {
         String in = "Сколько будет 2 + 2?";
         String result = DummyBot.answer(in);
         String expected = "Это ставит меня в тупик. Задайте другой вопрос.";
