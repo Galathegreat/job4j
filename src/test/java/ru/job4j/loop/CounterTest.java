bb2910dadea860d705d16ab49f3e8fe920fdcd40
XUXU <corpochta@gmail.com>
2022-05-26 12:10:09 +0300

    5.0.1. Сумма чисел. [#226416 #304566]

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -12,6 +12,7 @@ public class CounterTest {
         int expected = 30;
         Assert.assertEquals(expected, result);
     }
+
     @Test
     public void whenSumByEvenNumbersFromOneToTenThenThirty() {
         int start = 3;
