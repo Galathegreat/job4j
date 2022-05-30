d9ec9f69870af71a171eb308b852290dea2d96a7
XUXU <corpochta@gmail.com>
2022-05-30 14:15:45 +0300

    5.4. Протеиновая диета [#156313 #304645]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -1,9 +1,11 @@
 package ru.job4j.loop;
 
+import java.sql.SQLOutput;
+
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        while (nik <= ivan) {
+        while (nik > ivan) {
             ivan *= 3;
             nik *= 2;
             month++;
