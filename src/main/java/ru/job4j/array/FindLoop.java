9cf1fb1931a52d512c5c3fc7cfd05d963d605585
XUXU <corpochta@gmail.com>
2022-06-30 14:40:55 +0300

    6.1. Классический поиск перебором. [#33489]

---
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class FindLoop {
+    public static int indexOf(int[] data, int element) {
+        int rst = -1;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == element) {
+                rst = i;
+                break;
+            }
+
+        }
+        return rst;
+
+    }
+}
---
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FindLoopTest {
+
+    @Test
+    public void WhenArrayHas5then0() {
+        int[] data = {5, 4, 3, 2};
+        int element = 5;
+        int result = FindLoop.indexOf(data, element);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHasNot10ThenMinus1() {
+        int[] data = {5, 4, 3, 2};
+        int element = 10;
+        int result = FindLoop.indexOf(data, element);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHas3ThenTwo() {
+        int[] data = {5, 4, 3, 2};
+        int element = 3;
+        int result = FindLoop.indexOf(data, element);
+        int expected = 2;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHasNot6ThenMinus1() {
+        int[] data = {5, 4, 3, 2};
+        int element = 6;
+        int result = FindLoop.indexOf(data, element);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
+}
