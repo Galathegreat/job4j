42c044339099c6e9a70a3bf5cfac7133cbee6002
XUXU <corpochta@gmail.com>
2022-07-07 15:12:29 +0300

    6.7.2. Моно столбец в матрице. [#214127]

---
+++ src/main/java/ru/job4j/array/MonoVertical.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class MonoVertical {
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result = true;
+        for (char[] chars : board) {
+            if (chars[column] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
+
+
