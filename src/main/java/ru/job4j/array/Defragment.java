9cb4b11e58983a50e62c6e81e959f313cc7242a2
XUXU <corpochta@gmail.com>
2022-07-08 12:20:23 +0300

    6.8. Дефрагментация массива [#225 #319748]

--- src/main/java/ru/job4j/array/Defragment.java
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -6,9 +6,8 @@ public class Defragment {
             if (array[index] == null) {
                 for (int i = index + 1; i < array.length; i++) {
                     if (array[i] != null) {
-                        String swap = array[i];
+                        array[index] = array[i];
                         array[i] = null;
-                        array[index] = swap;
                         break;
                     }
                 }
