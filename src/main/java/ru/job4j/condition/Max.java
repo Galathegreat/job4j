d72587f247ad011caff384c44665c0799109048f
XUXU <corpochta@gmail.com>
2022-10-03 17:57:06 +0300

    3. Перегрузить метод max для трех чисел [#122644 #326914]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,11 +2,14 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        return left > right ? left : right;
+        return Math.max(left, right);
     }
 
-    public static int max(int a, int b, int c) {
-        return max(max(a, b), c);
-    }
-}
+    public static int max(int left, int right, int median) {
+            return max(left, max(right, median));
+        }
 
+        public static int max(int left, int right, int median, int finite) {
+            return max(max(left, right), max(median, finite));
+        }
+    }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -1,54 +1,53 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
 import org.junit.Test;
-
+import org.junit.Assert;
 public class MaxTest {
     @Test
-    public void whenMax1To2Then2() {
+    public void whenMax26ToMin13Then13() {
+        int left = 26;
+        int right = 13;
+        int result = Max.max(left, right);
+        int expected = 26;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax1ToMin2Then2() {
         int left = 1;
         int right = 2;
         int result = Max.max(left, right);
         int expected = 2;
         Assert.assertEquals(result, expected);
-
     }
 
     @Test
-    public void whenMax2To1Then2() {
-        int left = 2;
-        int right = 1;
+    public void whenBoth76Then76() {
+        int left = 76;
+        int right = 76;
         int result = Max.max(left, right);
-        int expected = 2;
+        int expected = 76;
         Assert.assertEquals(result, expected);
     }
 
     @Test
-    public void whenMax2To2Then2() {
-        int left = 2;
-        int right = 2;
-        int result = Max.max(left, right);
-        int expected = 2;
+    public void whenMax69To55ToMin34Then69() {
+        int left = 69;
+        int right = 55;
+        int median = 34;
+        int result = Max.max(left, right, median);
+        int expected = 69;
         Assert.assertEquals(result, expected);
     }
 
     @Test
-    public void whenA8B2C5Max8() {
-        int a = 3;
-        int b = 6;
-        int c = 2;
-        int result = Max.max(a, b, c);
-        int expected = 6;
-        Assert.assertEquals(result, expected);
-    }
-
-    @Test
-    public void whenA8B8C8Max8() {
-        int a = 1;
-        int b = 4;
-        int c = 10;
-        int result = Max.max(a, b, c);
-        int expected = 10;
+    public void whenMax161To98To70Then57() {
+        int left = 161;
+        int right = 98;
+        int median = 79;
+        int finite = 57;
+        int result = Max.max(left, right, median, finite);
+        int expected = 161;
         Assert.assertEquals(result, expected);
     }
 }
